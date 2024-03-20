2d68e3fa9ce71ee82375f19b394c23d62c094254
SuperninjaX2 <lunguj665@gmail.com>
2024-03-20 10:12:07 +0000

    new port

--- app.js
+++ app.js
@@ -1,41 +1,40 @@
 const express = require("express");
-const path = require("path")
-
+const path = require("path");
 
 const app = express();
-const PORT = 3000
+const PORT = process.env.PORT || 3000; // Use the port provided by Netlify or default to 3000
 app.use(express.static(path.join(__dirname, "frontend")));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 
-app.get('/',(req,res)=>{
-    res.render('index',{yes:"yes"})
-})
-app.get('/songs',(req,res)=>{
-    res.render("hymns")
-})
-app.get("/songs/:id",(req,res)=>{
-    const { id } = req.params;
-    const filePath = path.join(__dirname,'songs',`${id}.json`);
+app.get("/", (req, res) => {
+  res.render("index", { yes: "yes" });
+});
 
-    try {
-        const songData = require(filePath);
-        res.render('song',{"song":songData})
-    } catch (error) {
-        if (error.code === 'MODULE  NOT FOUND') {
-            res.status(404).send('song not found')
-        } else{
-            console.log(error)
-            res.status(500).send("error")
-        }
+app.get("/songs", (req, res) => {
+  res.render("hymns");
+});
+
+app.get("/songs/:id", (req, res) => {
+  const { id } = req.params;
+  const filePath = path.join(__dirname, "songs", `${id}.json`);
+
+  try {
+    const songData = require(filePath);
+    res.render("song", { song: songData });
+  } catch (error) {
+    if (error.code === "MODULE_NOT_FOUND") {
+      res.status(404).send("song not found");
+    } else {
+      console.error(error);
+      res.status(500).send("error");
     }
-})
-
-
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
- })
+});
 
- module.exports = app;
+module.exports = app;
